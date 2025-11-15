bced181b8fb60cac1bdef9741d266fc660830232
kairat <zhaksylykov.k06@gmail.com>
2025-11-15 16:50:43 +0500

    update schemas

---
+++ api/schemas.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+
+class Request(BaseModel):
+    Temperature: float
+    Humidity: float
+    PM25: float
+    PM10: float
+    NO2: float
+    CO: float
+    Proximity_to_Industrial_Areas: float
+    Population_Density: float
+
+class Response(BaseModel):
+    Air_Quality: str
